65d81238f19ff2451bbd19e92c16621a5f6ab329
DROPCICK <arseniykon1@gmail.com>
2023-12-29 16:58:17 +0200

    УПРАВЛЕНИЕ ПЕРСОНАЖЕМ

--- My project/Assets/scripts/DEKUTEST.cs
+++ My project/Assets/scripts/DEKUTEST.cs
@@ -6,12 +6,14 @@ public class DEKUTEST : MonoBehaviour
 
  {
         [SerializeField] float speed; //скорость персонажа
+        Animator anim; //Добавляем ссылку на аниматор
         Rigidbody rb; //ссылка на Rigidbody
         Vector3 direction; //Направление движения
 
         void Start()
         {
-            rb = GetComponent<Rigidbody>();
+          anim = GetComponent<Animator>();
+          rb = GetComponent<Rigidbody>();
         }
 
 
@@ -20,12 +22,33 @@ public class DEKUTEST : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         direction = transform.TransformDirection(x, 0, z);
+        //меняем анимацию персонажа
+        if (direction.magnitude > 0)
+        {
+           
+            anim.SetBool("run", true);
+        }
+        else anim.SetBool("run", false);
+        //
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("chappy" );
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetTrigger("box");
+        }
+
+
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(transform.position + direction * speed * Time.fixedDeltaTime);
+
     }
 
-
+        
 }
